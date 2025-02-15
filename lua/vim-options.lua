5c25146ddd852f38fd58cef41a64d12b3dc59a00
moad <moad2632005@gmail.com>
2025-02-15 18:40:00 +0100

    some minor changes in vim-options.lua

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,6 +7,7 @@ vim.opt.wrap = false               -- Disable line wrapping
 vim.opt.cursorline = true          -- Highlight the current line
 vim.g.mapleader = " "
 vim.g.background = "light"
+vim.opt.termguicolors = true
 
 vim.opt.swapfile = false
 
@@ -23,3 +24,16 @@ vim.opt.relativenumber = true      -- Show relative line numbers
 vim.opt.signcolumn = 'yes'         -- Always show sign column
 vim.opt.termguicolors = true       -- Enable true colors
 vim.opt.list = true                -- Show whitespace characters
+
+vim.opt.incsearch = true         -- Show search matches while typing
+vim.opt.hlsearch = true          -- Highlight search results
+vim.opt.ignorecase = true        -- Ignore case in search
+vim.opt.smartcase = true         -- But make search case-sensitive if uppercase letters are used
+
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard by default
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  command = "checktime"
+})
+
