c0633f998198bc62f861d36bf926ebc310f5c2e2
moad <moad2632005@gmail.com>
2025-02-16 21:20:08 +0100

    idk2

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,3 +1,11 @@
-return{
-  "rcarriga/nvim-notify",
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			stages = "fade_in_slide_out", -- or "fade", "slide", etc.
+			timeout = 3000, -- how long the notification stays
+			background_colour = "Normal", -- background color for notifications
+			minimum_width = 50, -- minimum width of the notification
+		})
+	end,
 }
