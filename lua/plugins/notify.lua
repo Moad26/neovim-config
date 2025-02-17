154655fdeed4ba95d3ebda36dfcb6b07e4aff1aa
moad <moad2632005@gmail.com>
2025-02-17 18:57:52 +0100

    nothing

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,7 +4,7 @@ return {
 		require("notify").setup({
 			stages = "fade_in_slide_out", -- or "fade", "slide", etc.
 			timeout = 3000, -- how long the notification stays
-			background_colour = "Normal", -- background color for notifications
+			background_colour = "#000000", -- background color for notifications
 			minimum_width = 50, -- minimum width of the notification
 		})
 	end,
