5bc7493ae93043498bd017ddc817332ddfa75f2d
moad <moad2632005@gmail.com>
2025-02-27 09:40:11 +0000

    -

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -3,19 +3,8 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
@@ -24,7 +13,5 @@ return {
         path = "/mnt/c/Users/moad2/Desktop/Obsidian/Study Data science",
       },
     },
-
-    -- see below for full list of options 👇
   },
 }
