47a9ef4c35bf830614a8ce16932ca5e733396ac5
moad <moad2632005@gmail.com>
2025-03-06 11:19:26 +0000

    idk 3

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,9 @@
 return {
   "folke/snacks.nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+    "nvim-tree/nvim-web-devicons",
+  },
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
@@ -13,13 +17,13 @@ return {
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    picker = { enabled = true },
-   -- notifier = { enabled = true },
+    -- picker = { enabled = true },
+    -- notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
-    util = {enabled=true},
+    util = { enabled = true },
   },
 }
