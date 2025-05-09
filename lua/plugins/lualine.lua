a7f3004c6bb2dccb9028f087e4b90222e292dfec
moad <moad2632005@gmail.com>
2025-05-09 23:40:53 +0100

    idk

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,26 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "auto",
-				-- no separators
-				section_separators = "",
-				component_separators = "",
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        icons_enabled = true,
+        -- no separators
+        section_separators = "",
+        component_separators = "",
 
-				-- dotted
-				--section_separators = { left = '•', right = '•' },
-				--component_separators = { left = '∙', right = '∙' },
-				--
-				--slang style
-				--section_separators = { left = '', right = '' },
-				--component_separators = { left = '', right = '' },
-				--
-				--arrows
-				--section_separators = { left = '', right = '' },
-				--component_separators = { left = '', right = '' },
-			},
-		})
-	end,
+        -- dotted
+        --section_separators = { left = '•', right = '•' },
+        --component_separators = { left = '∙', right = '∙' },
+        --
+        --slang style
+        --section_separators = { left = '', right = '' },
+        --component_separators = { left = '', right = '' },
+        --
+        --arrows
+        --section_separators = { left = '', right = '' },
+        --component_separators = { left = '', right = '' },
+      },
+    })
+  end,
 }
