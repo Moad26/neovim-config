42ab85e33d2858b7fd3ac7552ef520b90057e133
moad <moad2632005@gmail.com>
2025-08-29 15:34:41 +0100

    worked on lsp stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,16 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd", "bashls", "cmake" },
+        ensure_installed = {
+          "lua_ls", "pyright", "clangd", "bashls", "cmake",
+          -- Web dev LSPs
+          "ts_ls",       -- TypeScript/JavaScript
+          "html",        -- HTML
+          "cssls",       -- CSS
+          "tailwindcss", -- Tailwind CSS
+          "emmet_ls",    -- Emmet for HTML/CSS
+          "jsonls",      -- JSON
+        },
       })
     end,
   },
@@ -18,15 +27,50 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.clangd.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.cmake.setup({})
+
+      -- Get capabilities from nvim-cmp
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      -- Setup LSPs with capabilities
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.bashls.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { 'vim' } }
+          }
+        }
+      })
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.cmake.setup({ capabilities = capabilities })
+
+      -- Web dev LSPs
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" }
+      })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" }
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" }
+      })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+
+      -- Keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
     end,
   },
 }
+
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,74 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.markdownlint,
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.formatting.shellharden,
-        null_ls.builtins.formatting.beautysh,
-        null_ls.builtins.diagnostics.cmakelint,
-        null_ls.builtins.formatting.cmakelang,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"jay-babu/mason-null-ls.nvim",
+		"williamboman/mason.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				-- Python/Data Science
+				"black",
+				"ruff",
+				"mypy",
+				"isort",
+
+				-- C/C++
+				"clang-format",
+
+				-- Shell
+				"shellcheck",
+				"shellharden",
+				"beautysh",
+
+				-- CMake
+				"cmakelint",
+				"cmakelang",
+
+				-- Lua
+				"stylua",
+
+				-- Web Dev
+				"prettier", -- JS/TS/HTML/CSS/JSON formatter
+				"eslint_d", -- JS/TS linting
+				"stylelint", -- CSS linting
+			},
+			automatic_installation = true,
+		})
+
+		null_ls.setup({
+			sources = {
+				-- Python/Data Science
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.diagnostics.ruff,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.isort,
+
+				-- C/C++
+				null_ls.builtins.formatting.clang_format,
+
+				-- Shell
+				null_ls.builtins.diagnostics.shellcheck,
+				null_ls.builtins.formatting.shellharden,
+				null_ls.builtins.formatting.beautysh,
+
+				-- CMake
+				null_ls.builtins.diagnostics.cmakelint,
+				null_ls.builtins.formatting.cmakelang,
+
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+
+				-- Web Dev
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.diagnostics.stylelint,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
