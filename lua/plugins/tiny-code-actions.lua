9d057b63369850f5f1a4d23137fb9877e8898b5f
Moad26 <moad2632005@gmail.com>
2025-11-29 20:30:50 +0100

    idk

--- lua/plugins/tiny-code-actions.lua
+++ lua/plugins/tiny-code-actions.lua
@@ -1,13 +1,7 @@
 return {
 	"rachartier/tiny-code-action.nvim",
 	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-
-		-- optional picker via telescope
-		{ "nvim-telescope/telescope.nvim" },
-		-- optional picker via fzf-lua
-		{ "ibhagwan/fzf-lua" },
-		-- .. or via snacks
+		"ibhagwan/fzf-lua",
 		{
 			"folke/snacks.nvim",
 			opts = {
